16f6ddd636eae90fad2cbcc2775046191be8b5b6
Max <vlad@itpw.ru>
2022-07-01 10:29:13 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import { MainPage } from './pages/MainPage';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <Routes>
+            <Route
+              path="/"
+              element={
+                <MainPage />
+              }
+            />
+          </Routes>
     </div>
   );
 }
