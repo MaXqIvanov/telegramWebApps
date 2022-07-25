d47528721682939e42a9bda52e1b321c79d63881
“MaXqIvanov” <maksivanov35@ya.ru>
2022-07-25 10:43:02 +0300

    update and fix

--- Bots/index.js
+++ Bots/index.js
@@ -1,80 +1,84 @@
 const { Telegraf } = require('telegraf')
 require('dotenv').config()
-const express = require('express');
+const express = require('express')
 const { default: axios } = require('axios');
 const expressApp = express()
 
-let PORT = process.env.PORT | 3000;
+const port = process.env.PORT || 3000
+expressApp.get('/', (req, res) => {
+  res.send('Bot!')
+})
+expressApp.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
 
-const bot = new Telegraf('5460453079:AAECrP-8PyXD4vIcDeV7fpgMuBcbhilano8', { webHook : { PORT : PORT} })
-
-// bot.hears(/./, (ctx) => ctx.reply('Welcome', {reply_markup:{keyboard:[[{ text: 'заказать', web_app: {url: web_link}}]]}}))
+const bot = new Telegraf('5460453079:AAECrP-8PyXD4vIcDeV7fpgMuBcbhilano8', { webHook : { PORT : port} })
 
 bot.hears(/./, async (ctx) =>{ 
-    const chat = ctx.message
-    const web_link = `https://lambent-praline-e00ab7.netlify.app/`
-    let chat_id;
-    let user_id;
-    console.log(ctx.match);
-    console.log(ctx);
+  const chat = ctx.message
+  const web_link = `https://lambent-praline-e00ab7.netlify.app/`
+  let chat_id;
+  let user_id;
+  console.log(ctx.match);
+  console.log(ctx);
 
-    
+  
 
-    if(ctx.match.input.includes('/start ')){
-        chat_id = chat.text.split(' ')
-        user_id = chat.from.id;
-        // delete this line
-        // end delete this line
-        await axios.post(`https://everyservices.itpw.ru/chat/telegram_chat/`,{
-            "user": String(user_id),
-            "chat": String(chat_id)
-        }).catch((e)=> {}).then((response)=> console.log(response))
-    }
+  if(ctx.match.input.includes('/start ')){
+      chat_id = chat.text.split(' ')
+      user_id = chat.from.id;
+      // delete this line
+      // end delete this line
+      await axios.post(`https://everyservices.itpw.ru/chat/telegram_chat/`,{
+          "user": String(user_id),
+          "chat": String(chat_id)
+      }).catch((e)=> {}).then((response)=> console.log(response))
+  }
 try {
-    if(ctx.match.input.includes('/start')){
-        await ctx.telegram.sendMessage(chat.from.id, `Здраствуйте ${chat.from.first_name}!, Вы собираитесь записаться на услуги к мастеру, для этого нажмите на кнопку ниже`, {
-            reply_markup: { 
-                selective: true,
-                resize_keyboard: true,
-                inline_keyboard: [[{ text: `Записаться на услугу ❗`, web_app: {url: web_link}}]]
-                },
-                parse_mode: 'Markdown',
-        })    
-    }
-    if(ctx.match.input.includes('/post')){
-        let text = ctx.update.message.text.split('/post')
-        text = text[1]
-        await ctx.deleteMessage(ctx.message.message_id)   
-        ctx.telegram.sendMessage(ctx.chat.id, `${text}`, {
-            reply_markup: {
-                resize_keyboard: true,
-                parse_mode: 'Markdown',
-                inline_keyboard: [
-                    [{text: 'Записаться к мастеру ❗', url: `https://t.me/IT_Power_new_bot?start=${ctx.update.message.chat.username}`}]
-                ]
-            }
-        })
-    }
+  if(ctx.match.input.includes('/start')){
+      await ctx.telegram.sendMessage(chat.from.id, `Здраствуйте ${chat.from.first_name}!, Вы собираитесь записаться на услуги к мастеру, для этого нажмите на кнопку ниже`, {
+          reply_markup: { 
+              selective: true,
+              resize_keyboard: true,
+              inline_keyboard: [[{ text: `Записаться на услугу ❗`, web_app: {url: web_link}}]]
+              },
+              parse_mode: 'Markdown',
+      })    
+  }
+  if(ctx.match.input.includes('/post')){
+      let text = ctx.update.message.text.split('/post')
+      text = text[1]
+      await ctx.deleteMessage(ctx.message.message_id)   
+      ctx.telegram.sendMessage(ctx.chat.id, `${text}`, {
+          reply_markup: {
+              resize_keyboard: true,
+              parse_mode: 'Markdown',
+              inline_keyboard: [
+                  [{text: 'Записаться к мастеру ❗', url: `https://t.me/IT_Power_new_bot?start=${ctx.update.message.chat.username}`}]
+              ]
+          }
+      })
+  }
 
 
 
-    //?? block for delete messages //!! important   need work for this func
-    
-    for(let i = 0; i <= 200; i++ ){
-        if(ctx.message.from.id === ctx.message.chat.id){
-            try {
-                // console.log(ctx);
-                // console.log({...ctx.message});
-                k =  ctx.message.message_id-i;
-                await ctx.deleteMessage(k)   
-            } catch (error) {
-                
-            }
-        }
-    }
-    //?? block for delete messages
-    } catch (error) {
-    //console.log(error);
+  //?? block for delete messages //!! important   need work for this func
+  
+  for(let i = 0; i <= 200; i++ ){
+      if(ctx.message.from.id === ctx.message.chat.id){
+          try {
+              // console.log(ctx);
+              // console.log({...ctx.message});
+              k =  ctx.message.message_id-i;
+              await ctx.deleteMessage(k)   
+          } catch (error) {
+              
+          }
+      }
+  }
+  //?? block for delete messages
+  } catch (error) {
+  //console.log(error);
 }
 
 },
@@ -82,21 +86,21 @@ try {
 
 // this function need if admin send photo for post 
 bot.on('message', async msg => {
-    if(msg.update.message?.caption?.includes('/img')){
-        let text = msg.update.message.caption.split('/img')
-        text = text[1]
-        await msg.deleteMessage(msg.update.message.message_id)   
-        msg.telegram.sendPhoto(msg.update.message.chat.id, msg.update.message?.photo[0]?.file_id, {
-            caption: `${text}`,
-            reply_markup: {
-                resize_keyboard: true,
-                parse_mode: 'Markdown',
-                inline_keyboard: [
-                    [{text: 'Записаться к мастеру ❗', url: `https://t.me/IT_Power_new_bot?start=${msg.update.message.chat.username}`}]
-                ]
-            }
-        })
-    }
+  if(msg.update.message?.caption?.includes('/img')){
+      let text = msg.update.message.caption.split('/img')
+      text = text[1]
+      await msg.deleteMessage(msg.update.message.message_id)   
+      msg.telegram.sendPhoto(msg.update.message.chat.id, msg.update.message?.photo[0]?.file_id, {
+          caption: `${text}`,
+          reply_markup: {
+              resize_keyboard: true,
+              parse_mode: 'Markdown',
+              inline_keyboard: [
+                  [{text: 'Записаться к мастеру ❗', url: `https://t.me/IT_Power_new_bot?start=${msg.update.message.chat.username}`}]
+              ]
+          }
+      })
+  }
 })
 
 
@@ -120,4 +124,4 @@ bot.startPolling()
 // ** пост отправится от имени бота и перезапишет пост отправленный пользователем
 // ** /img - для публикации поста с картинкой - записывается в блоке с Заголовком /caption
 
-// 
+// отловить ошибку у бота - с сервером 
